25426310ed92f5776c47526b0587ab1533a08be7
MarcHernX15 <drstein21@live.com>
2024-07-17 16:06:55 -0500

    Script Change
    update

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -7,7 +7,7 @@ public class Bullet : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Destroy(gameObject, 10);
     }
 
     // Update is called once per frame
@@ -21,6 +21,12 @@ public class Bullet : MonoBehaviour
         if(collision.gameObject.CompareTag("Target"))
         {
             Destroy(collision.gameObject);
+            Destroy(gameObject);
+        }
+
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            
             Destroy(gameObject);
         }
     }
