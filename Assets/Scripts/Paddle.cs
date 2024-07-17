99a5f021497eddd07132a89f339a8b45c98e7ff9
MarcHernX15 <drstein21@live.com>
2024-07-17 17:43:20 -0500

    Bug Fixes 07/17/2024

--- Assets/Scripts/Paddle.cs
+++ Assets/Scripts/Paddle.cs
@@ -29,6 +29,7 @@ public class Paddle : MonoBehaviour
         }
         else 
         {
+            
             movement = Input.GetAxisRaw("Vertical2");
             if (Input.GetKeyDown(KeyCode.RightControl))
             {
